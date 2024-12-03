601a3fdc6060fc867dc18ef738c81971359a9eec
Abhik004 <abhiksamanta004@gmail.com>
2024-12-03 17:39:19 +0530

    progress in user ui

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -4,24 +4,24 @@ const LocationSearchPanel = () => {
   return (
     <div>
       {/* Sample Data */}
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+      <div className='flex gap-4 border-2 p-3 border-white active:border-black items-center my-2 justify-start'>
+        <h2 className='bg-[#eee] h-8 w-12 flex items-center rounded-xl justify-center '><i className="ri-map-pin-fill"></i></h2>
         <h4 className='font-medium'>24B, Near Kapoor's cafe, Patia, KIIT, Bhubaneswar</h4>
       </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+      <div className='flex gap-4 border-2 p-3 border-white active:border-black items-center my-2 rounded-xl justify-start'>
+        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
         <h4 className='font-medium'>24B, Near Kapoor's cafe, Patia, KIIT, Bhubaneswar</h4>
       </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+      <div className='flex gap-4 border-2 p-3 border-white active:border-black items-center my-2 rounded-xl justify-start'>
+        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe, Patia, KIIT, Bhubaneswar</h4>
+      </div> 
+      <div className='flex gap-4 border-2 p-3 border-white active:border-black items-center my-2 rounded-xl justify-start'>
+        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
         <h4 className='font-medium'>24B, Near Kapoor's cafe, Patia, KIIT, Bhubaneswar</h4>
       </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        <h4 className='font-medium'>24B, Near Kapoor's cafe, Patia, KIIT, Bhubaneswar</h4>
-      </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+      <div className='flex gap-4 border-2 p-3 border-white active:border-black items-center my-2 rounded-xl justify-start'>
+        <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
         <h4 className='font-medium'>24B, Near Kapoor's cafe, Patia, KIIT, Bhubaneswar</h4>
       </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
   },[panelOpen])
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className='w-16 absolute left-5 top-5' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png'/>
       <div className='h-screen w-screen'>
       {/* Image for temporary use */}
@@ -75,7 +75,36 @@ const Home = () => {
         <div ref={panelRef} className='bg-white opacity-0 h-0'>
           <LocationSearchPanel/>
         </div>
-        
+        <div className='fixed w-full z-10 bottom-0 translate-y-full px-3 py-8 bg-white'>
+          <h3 className='text-2xl font-semibold mb-5'>Choose a vehicle!</h3>
+          <div className='flex border-2 w-full  active:border-black p-3 mb-2 rounded-xl items-center justify-between'>
+            <img className='h-12' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdc1aAZzBJjW2GhT0TquUROYNGjc-8j6cSNeafEQAIpAOFJPXdwH_DrnzTOVen5HoFDRc&usqp=CAU'/>
+            <div className='ml-2 w-1/2'>
+              <h4 className='font-medium text-base'>xGO <span><i className="ri-user-3-fill"></i>4</span></h4>
+              <h5 className='font-medium text-sm'>3 mins away</h5>
+              <p className='font-normal text-xs texxt-gray-600'>Affordable, compact rides</p>
+            </div>
+            <h2 className='text-lg font-semibold'>₹193.20</h2>
+          </div>
+          <div className='flex border-2 w-full active:border-black p-3 mb-2 rounded-xl items-center justify-between'>
+            <img className='h-12' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png'/>
+            <div className='ml-2 w-1/2'>
+              <h4 className='font-medium text-base'>xMOTO <span><i className="ri-user-3-fill"></i>1</span></h4>
+              <h5 className='font-medium text-sm'>7 mins away</h5>
+              <p className='font-normal text-xs texxt-gray-600'>Affordable, motorcycle rides</p>
+            </div>
+            <h2 className='text-lg font-semibold'>₹65.70</h2>
+          </div>
+          <div className='flex border-2 w-full  active:border-black p-3 mb-2 rounded-xl items-center justify-between'>
+            <img className='h-12' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png'/>
+            <div className='ml-2 w-1/2'>
+              <h4 className='font-medium text-base'>xAUTO <span><i className="ri-user-3-fill"></i>3</span></h4>
+              <h5 className='font-medium text-sm'>7 mins away</h5>
+              <p className='font-normal text-xs texxt-gray-600'>Affordable, auto rides</p>
+            </div>
+            <h2 className='text-lg font-semibold'>₹118.80</h2>
+          </div>
+        </div>
       </div>
     </div>
   )
